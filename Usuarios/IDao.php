d3dd72655c418a149e0f4e820e0c8790d616eb73
Mari2390 <158357913+Mari2390@users.noreply.github.com>
2024-02-21 21:36:32 -0600

    Eliminar carpeta

--- Usuarios/IDao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-interface IDao {
-    public function buscarTodos();
-    public function buscar($id);
-    public function insertar(Usuario $usuario);
-    public function actualizar(Usuario $usuario);
-    public function eliminar($id);
-}
